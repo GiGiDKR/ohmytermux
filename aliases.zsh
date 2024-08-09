5b19bce0ea3f1ccc6531e38e5dc30f476bad12c8
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-08-09 11:37:15 +0200

    Update v2

--- aliases.zsh
+++ aliases.zsh
@@ -28,3 +28,6 @@ alias prop='nano $HOME/.termux/termux.properties'
 alias gc='git clone'
 alias tmx='cd $HOME/.termux'
 alias cm='chmod +x'
+alias push="git pull && git add . && git commit -m 'mobile push>
+alias zshconfig="nano ~/.zshrc"
+alias ohmyzsh="nano ~/.oh-my-zsh"
